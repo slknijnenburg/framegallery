bfd4cf93aa16763ba319f3fd6e004b02fbb2f1bd
Sebastiaan Knijnenburg <sebastiaan@castoredc.com>
2024-10-06 23:14:06 +0200

    Link `View` button to POST active-art endpoint

--- ui/src/components/ArtItemCard.tsx
+++ ui/src/components/ArtItemCard.tsx
@@ -8,12 +8,23 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import DeleteIcon from '@mui/icons-material/Delete';
+import axios from "axios";
 
 export interface ArtItemCardProps {
     item: ArtItem;
 }
 
 export default function ArtItemCard({item}: ArtItemCardProps) {
+    // Create function to POST to the API to make this art active, that will be called when the button is clicked
+    const makeActiveArt = async () => {
+        try {
+            await axios.post(`http://localhost:7999/api/active-art/${item.content_id}`);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
     return (
         <Card sx={{maxWidth: 345}}>
             <CardMedia
@@ -30,6 +41,7 @@ export default function ArtItemCard({item}: ArtItemCardProps) {
                 <Button variant="contained"
                         startIcon={<VisibilityIcon/>}
                         size="small"
+                        onClick={makeActiveArt}
                 >
                     View
                 </Button>
