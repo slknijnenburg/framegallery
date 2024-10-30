113d3e2b27dc6265b07b1789de8bbc25f5f03985
Sebastiaan Knijnenburg <sebastiaan@castoredc.com>
2024-10-30 17:39:21 +0100

    Add alembic for database migrations
    Start with a new DB file for a fresh start.
    Adds an Image model.

--- framegallery/models.py
+++ framegallery/models.py
@@ -2,6 +2,9 @@ from sqlalchemy import Boolean, Column, Integer, String
 
 from .database import Base
 
+"""
+Representation of an Art item on the Samsung television
+"""
 class ArtItem(Base):
     __tablename__ = "art_items"
     content_id = Column(String, primary_key=True)
@@ -20,8 +23,18 @@ class ArtItem(Base):
     thumbnail_filetype = Column(String)
     thumbnail_data = Column(String)
 
-
 #art_contents: List[ArtContent] = [
 #    {"content_id": "MY_F0003", "category_id": "MY-C0002", "slideshow": False, "matte_id": "none", "portrait_matte_id": "shadowbox_black", "width": 1920, "height": 1080, "image_date": "2024:10:02 12:08:25", "content_type": "mobile"},
 #    {"content_id": "MY_F0002", "category_id": "MY-C0002", "slideshow": False, "matte_id": "shadowbox_polar", "portrait_matte_id": "shadowbox_polar", "width": 1920, "height": 1280, "image_date": "2024:10:02 11:58:36", "content_type": "mobile"}
 #]
+
+class Image(Base):
+    __tablename__ = "images"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    filename = Column(String, index=True)
+    filepath = Column(String, index=True)
+    filetype = Column(String)
+    width = Column(Integer)
+    height = Column(Integer)
+    aspect_width = Column(Integer)
+    aspect_height = Column(Integer)
