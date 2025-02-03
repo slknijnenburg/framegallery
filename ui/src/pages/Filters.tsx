b5be88471410e8d4ae6bd16ce4d4e9a247c49e33
Sebastiaan Knijnenburg <sebastiaan@castoredc.com>
2025-02-03 23:22:36 +0100

    Make Save Filter button work as expected
    Always update when Save is clicked, don't duplicate or
    add a new filter. The only way to add a new filter is via
    the + button and that dialog.

--- ui/src/pages/Filters.tsx
+++ ui/src/pages/Filters.tsx
@@ -19,19 +19,19 @@ import {
 import React, { ReactNode, useState } from 'react';
 import FilterBuilder from '../components/Filters/FilterBuilder';
 import { Filter } from '../components/Filters/Filter';
-import { Add as AddIcon, Delete as DeleteIcon, Save as SaveIcon } from '@mui/icons-material';
-// import { RuleGroupType } from 'react-querybuilder';
-//
-// const testFilters = [
-//   {
-//     id: '1',
-//     name: '16:9 aspect ratio',
-//     query:
-//       '{"id":"root","combinator":"and","rules":[{"id":"b06c8a69-747e-4e37-acb0-e8e9210e801e","field":"aspect_ratio_width","operator":"=","valueSource":"value","value":"16"},{"id":"d7360b89-0cac-4f23-bd2c-318778f08e09","field":"aspect_ratio_height","operator":"=","valueSource":"value","value":"9"}]}',
-//   },
-//   { id: '2', name: 'Holiday photos' },
-//   { id: '3', name: 'Wedding' },
-// ];
+import { Add as AddIcon, Delete as DeleteIcon } from '@mui/icons-material';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const testFilters = [
+  {
+    id: '1',
+    name: '16:9 aspect ratio',
+    query:
+      '{"id":"root","combinator":"and","rules":[{"id":"b06c8a69-747e-4e37-acb0-e8e9210e801e","field":"aspect_ratio_width","operator":"=","valueSource":"value","value":"16"},{"id":"d7360b89-0cac-4f23-bd2c-318778f08e09","field":"aspect_ratio_height","operator":"=","valueSource":"value","value":"9"}]}',
+  },
+  { id: '2', name: 'Holiday photos' },
+  { id: '3', name: 'Wedding' },
+];
 
 const Filters = () => {
   const [filters, setFilters] = useState<Filter[]>([]);
@@ -44,21 +44,8 @@ const Filters = () => {
     const filterId = event.target.value;
     const filter = filters.find((filter) => filter.id === filterId);
     if (filter) {
-      console.log('Filter changed to:', filter);
       setSelectedFilter(filter);
     }
-  };
-
-  // TODO add support to delete filters
-  const handleSaveFilter = (name: string, rule: string) => {
-    // TODO add support to update existing filters
-    const newFilter = {
-      id: (filters.length + 1).toString(),
-      name: name,
-      query: rule,
-    };
-    setFilters([...filters, newFilter]);
-    setSelectedFilter(newFilter);
   };
 
   const handleCreateNewFilter = () => {
@@ -98,8 +85,6 @@ const Filters = () => {
           <CardContent>
             <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
               <Select
-                // value={selectedFilter}
-                // onChange={(e) => setSelectedFilter(e.target.value)}
                 value={selectedFilter?.id || filters[0]?.id || ''}
                 onChange={handleFilterChange}
                 displayEmpty
@@ -124,9 +109,6 @@ const Filters = () => {
                   <IconButton color="error" onClick={() => handleDeleteFilter(selectedFilter.id)} size="small">
                     <DeleteIcon />
                   </IconButton>
-                  <IconButton color="primary" onClick={() => handleSaveFilter} size="small">
-                    <SaveIcon />
-                  </IconButton>
                 </>
               )}
             </Box>
@@ -149,12 +131,8 @@ const Filters = () => {
                   border: '2px dashed rgba(0, 0, 0, 0.12)',
                 }}
               >
-                {/*<Typography color="text.secondary">*/}
-                {/*    Query Builder Goes Here*/}
-                {/*</Typography>*/}
                 <FilterBuilder
                   selectedFilter={selectedFilter}
-                  saveFilterHandler={handleSaveFilter}
                   updateFilterHandler={handleUpdateFilter}
                 />
               </Paper>
@@ -185,38 +163,6 @@ const Filters = () => {
           </DialogActions>
         </Dialog>
       </Box>
-
-      {/*<Container maxWidth="xl">*/}
-      {/*    <Grid container spacing={2}>*/}
-      {/*        <Grid size={6}>*/}
-      {/*            <h1>Filters</h1>*/}
-      {/*        </Grid>*/}
-      {/*        <Grid size={12}>*/}
-      {/*            <FormControl fullWidth>*/}
-      {/*                <InputLabel id="filter-select-label">Filter</InputLabel>*/}
-      {/*                <Select*/}
-      {/*                    labelId="filter-select-label"*/}
-      {/*                    id="filter-select"*/}
-      {/*                    label="Filter"*/}
-      {/*                    value={selectedFilter?.id || filters[0]?.id || ''}*/}
-      {/*                    onChange={handleFilterChange}*/}
-      {/*                >*/}
-      {/*                    {*/}
-      {/*                        filters.map((filter) => (*/}
-      {/*                            <MenuItem key={filter.id} value={filter.id}>{filter.name}</MenuItem>*/}
-      {/*                        ))*/}
-      {/*                    }*/}
-      {/*                </Select>*/}
-      {/*            </FormControl>*/}
-      {/*        </Grid>*/}
-      {/*        <Grid size={12}/>*/}
-      {/*    </Grid>*/}
-      {/*    <FilterBuilder*/}
-      {/*        selectedFilter={selectedFilter}*/}
-      {/*        saveFilterHandler={handleSaveFilter}*/}
-      {/*        updateFilterHandler={handleUpdateFilter}*/}
-      {/*    />*/}
-      {/*</Container>*/}
     </>
   );
 };
