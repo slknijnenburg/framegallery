103154e0e0e44f477d14f99ebc38a44841721c87
Sebastiaan Knijnenburg <sebastiaan@castoredc.com>
2025-02-23 21:29:13 +0100

    Fix all Ruff errors and warnings

--- framegallery/configuration/update_current_active_image_config_listener.py
+++ framegallery/configuration/update_current_active_image_config_listener.py
@@ -1,19 +1,22 @@
-from datetime import datetime
+import datetime
 
 from blinker import signal
 
 from framegallery.models import Image
-from framegallery.repository.config_repository import ConfigRepository, ConfigKey
+from framegallery.repository.config_repository import ConfigKey, ConfigRepository
 
 
 class UpdateCurrentActiveImageConfigListener:
-    def __init__(self, config_repository: ConfigRepository):
+    """Update the current active image configuration when the active image changes."""
+
+    def __init__(self, config_repository: ConfigRepository) -> None:
         self._config_repository = config_repository
         self._active_image_updated_signal = signal("active_image_updated")
         self._active_image_updated_signal.connect(self._on_active_image_updated)
 
-    async def _on_active_image_updated(self, sender, active_image: Image):
+    async def _on_active_image_updated(self, active_image: Image) -> None:
+        """Update the current active image configuration in the database."""
         self._config_repository.set(ConfigKey.CURRENT_ACTIVE_IMAGE, active_image.id)
         self._config_repository.set(
-            ConfigKey.CURRENT_ACTIVE_IMAGE_SINCE, datetime.now().isoformat()
+            ConfigKey.CURRENT_ACTIVE_IMAGE_SINCE, datetime.datetime.now(tz=datetime.UTC).isoformat()
         )
--- framegallery/repository/filter_repository.py
+++ framegallery/repository/filter_repository.py
@@ -1,31 +1,36 @@
-from typing import Optional
 
-from sqlalchemy import select, Sequence
+from sqlalchemy import Sequence, select
 from sqlalchemy.orm import Session
 
 from framegallery.models import Filter
 
 
 class FilterRepository:
-    def __init__(self, db: Session):
+    """Manages the filters in the database."""
+
+    def __init__(self, db: Session) -> None:
         self._db = db
 
-    def get_filters(self) -> Optional[Sequence[Filter]]:
+    def get_filters(self) -> Sequence[Filter] | None:
+        """Get all filters from the database."""
         stmt = select(Filter).order_by(Filter.name)
 
         return self._db.execute(stmt).scalars().all()
 
-    def get_filter_by_name(self, name: str) -> Optional[Filter]:
+    def get_filter_by_name(self, name: str) -> Filter | None:
+        """Get a filter by its name."""
         stmt = select(Filter).where(Filter.name == name)
 
         return self._db.execute(stmt).scalar()
 
-    def get_filter(self, filter_id: int) -> Optional[Filter]:
+    def get_filter(self, filter_id: int) -> Filter | None:
+        """Get a filter by its ID."""
         stmt = select(Filter).where(Filter.id == filter_id)
 
         return self._db.execute(stmt).scalar_one_or_none()
 
     def create_filter(self, name: str, query: str) -> Filter:
+        """Create a new filter."""
         filter_ = Filter(name=name, query=query)
 
         self._db.add(filter_)
