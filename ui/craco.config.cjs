94c2907110eae19511ee4d45ee48247566869cae
Sebastiaan Knijnenburg <sebastiaan@castoredc.com>
2025-05-07 23:41:21 +0200

    Use craco to allow upgrade to typescript 5

---
+++ ui/craco.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  // webpack: { /* ... */ },
+  // babel: { /* ... */ },
+  // eslint: { /* ... */ },
+  // jest: { /* ... */ },
+  // typescript: {
+  //   enableTypeChecking: true /* (default value)  */
+  // },
+  // style: {
+  //   postcssOptions: {
+  //     plugins: [
+  //       require('tailwindcss'),
+  //       require('autoprefixer'),
+  //     ],
+  //   },
+  // },
+  // devServer: { /* ... */ },
+  // plugins: [
+  //   { plugin: require('craco-less'), options: { /* ... */ } },
+  // ]
+};
