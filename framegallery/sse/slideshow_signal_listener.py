835bcf8de0a5f72a3f4a681dd6e2f8a17196472a
Sebastiaan Knijnenburg <sebastiaan@castoredc.com>
2025-05-19 10:36:09 +0200

    feat: implement server-sent events for real-time slideshow updates

---
+++ framegallery/sse/slideshow_signal_listener.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+
+from blinker import signal
+
+from framegallery.models import Image
+
+logger = logging.getLogger("framegallery")
+
+
+class SlideshowSignalSSEListener:
+    """Listens for slideshow signals and puts events onto an SSE queue."""
+
+    def __init__(self, event_queue: asyncio.Queue) -> None:
+        self._event_queue = event_queue
+        self._active_image_updated_signal = signal("active_image_updated")
+        self._active_image_updated_signal.connect(self._on_active_image_updated)
+
+    async def _on_active_image_updated(self, _: object, active_image: Image) -> None:
+        """Handle the active_image_updated signal and put event onto the SSE queue."""
+        if self._event_queue:
+            event_data = {"event": "slideshow_update", "imageId": active_image.id}
+            await self._event_queue.put(event_data)
+            logger.debug(
+                "SlideshowSignalSSEListener: Put event on queue: %s", event_data
+            )
